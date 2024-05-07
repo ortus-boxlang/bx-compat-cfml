d41e51a76c60f483f6e7ce2b5ad46116ccaa9761
Luis Majano <lmajano@gmail.com>
2024-05-07 18:29:44 +0200

    get engine properties done

---
+++ src/main/java/ortus/boxlang/modules/compat/bifs/cache/CacheGetEngineProperties.java
@@ -0,0 +1,44 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package ortus.boxlang.modules.compat.bifs.cache;
+
+import ortus.boxlang.runtime.bifs.BIF;
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.scopes.ArgumentsScope;
+import ortus.boxlang.runtime.types.Struct;
+
+@BoxBIF
+public class CacheGetEngineProperties extends BIF {
+
+	/**
+	 * Constructor
+	 */
+	public CacheGetEngineProperties() {
+		super();
+	}
+
+	/**
+	 * Returns some properties of the cache engine
+	 *
+	 * @param context   The context in which the BIF is being invoked.
+	 * @param arguments Argument scope for the BIF.
+	 *
+	 * @return A struct containing the properties of the cache engine
+	 */
+	public Object _invoke( IBoxContext context, ArgumentsScope arguments ) {
+		return Struct.of( "name", "BoxLang" );
+	}
+}
