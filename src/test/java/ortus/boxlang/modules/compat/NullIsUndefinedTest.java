dee90ff1194a82fac3592433f35a29c1930b97f6
Brad Wood <brad@bradwood.com>
2024-11-01 00:04:29 -0500

    BL-697 BL-638 BL-669

---
+++ src/test/java/ortus/boxlang/modules/compat/NullIsUndefinedTest.java
@@ -0,0 +1,64 @@
+package ortus.boxlang.modules.compat;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.types.exceptions.KeyNotFoundException;
+
+/**
+ * This loads the module and runs an integration test on the module.
+ */
+public class NullIsUndefinedTest extends BaseIntegrationTest {
+
+	@DisplayName( "Test null is undefined" )
+	@Test
+	public void testNullIsUndefined() {
+		loadModule();
+
+		// @formatter:off
+		Throwable t = assertThrows( KeyNotFoundException.class, () -> runtime.executeSource( """
+			foo = null;
+			result = foo;
+			"""
+			, context ) );
+		// @formatter:on
+		assertThat( t.getMessage() ).contains( "The requested key [foo] was not located" );
+	}
+
+	@DisplayName( "Test null scope lookup order" )
+	@Test
+	public void testNullScopeLookupOrder() {
+		loadModule();
+
+		// @formatter:off
+		runtime.executeSource( """
+			function testMe( string foo ) {
+				local.foo = null;
+				// local scope is checked first, but since local is null, we'll ignore it
+				return foo;
+			}
+			result = testMe( "arguments" );
+			"""
+			, context );
+		// @formatter:on
+		assertThat( variables.get( result ) ).isEqualTo( "arguments" );
+	}
+
+	@DisplayName( "It tests that null exists" )
+	@Test
+	public void testNull() {
+		loadModule();
+
+		// @formatter:off
+		runtime.executeSource(
+		    """
+		    myStruct={ "foo" : null };
+		    result = myStruct.keyExists( "foo" );
+		    """,
+		    context );
+		assertThat( variables.getAsBoolean( result ) ).isFalse();
+	}
+}
