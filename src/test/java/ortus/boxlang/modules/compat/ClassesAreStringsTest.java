dc62ecb91c55ce0ddec117ed978bacec4dcf4cd1
Jon Clausen <jclausen@ortussolutions.com>
2025-03-29 12:38:37 -0400

    remove no longer applicable tests

--- src/test/java/ortus/boxlang/modules/compat/ClassesAreStringsTest.java
+++ src/test/java/ortus/boxlang/modules/compat/ClassesAreStringsTest.java
@@ -5,7 +5,6 @@ import static com.google.common.truth.Truth.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import ortus.boxlang.compiler.parser.BoxSourceType;
 import ortus.boxlang.runtime.dynamic.casters.StringCaster;
 
 /**
@@ -19,22 +18,6 @@ public class ClassesAreStringsTest extends BaseIntegrationTest {
 		String myStr = StringCaster.cast( "".getClass() );
 		assertThat( myStr ).isInstanceOf( String.class );
 		assertThat( myStr ).isEqualTo( "java.lang.String" );
-	}
-
-	@DisplayName( "It can use string BIF" )
-	@Test
-	void testItCanUseStringBIF() {
-		runtime.executeSource( """
-		                       	result = listLast( "Hello, World".getClass(), '.' );
-		                       	result2 = "Hello, World".getClass().listLast( '.' );
-		                       	result3 = "foo #'brad'.getClass()# bar";
-		                       	result4 = "foo " & 'brad'.getClass() & " bar";
-		                       """,
-		    context, BoxSourceType.CFSCRIPT );
-		assertThat( variables.get( result ) ).isEqualTo( "String" );
-		assertThat( variables.get( "result2" ) ).isEqualTo( "String" );
-		assertThat( variables.get( "result3" ) ).isEqualTo( "foo java.lang.String bar" );
-		assertThat( variables.get( "result4" ) ).isEqualTo( "foo java.lang.String bar" );
 	}
 
 }
