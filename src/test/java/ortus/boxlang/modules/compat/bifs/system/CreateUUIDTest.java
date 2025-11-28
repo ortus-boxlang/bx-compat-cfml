f2da557db339a51c031170dd6ae24c45d4eb39f8
Michael Born <michael@ortussolutions.com>
2025-11-28 11:23:47 -0500

    BL-1979 - Move CFML-style UUID format to bx-compat

---
+++ src/test/java/ortus/boxlang/modules/compat/bifs/system/CreateUUIDTest.java
@@ -0,0 +1,97 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ortus.boxlang.modules.compat.bifs.system;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import ortus.boxlang.runtime.BoxRuntime;
+import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
+import ortus.boxlang.runtime.scopes.IScope;
+import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.scopes.VariablesScope;
+
+public class CreateUUIDTest {
+
+	static BoxRuntime	instance;
+	IBoxContext			context;
+	IScope				variables;
+	static Key			result	= new Key( "result" );
+
+	@BeforeAll
+	public static void setUp() {
+		instance = BoxRuntime.getInstance( true );
+	}
+
+	@BeforeEach
+	public void setupEach() {
+		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
+		variables	= context.getScopeNearby( VariablesScope.name );
+	}
+
+	@DisplayName( "It returns a bastardized UUID format to match ACF and Lucee" )
+	@Test
+	public void testUUIDFormatIsCFMLTraditional() {
+		instance.executeSource(
+		    """
+		    result = createUUID();
+		    """,
+		    context );
+		String uuid = variables.getAsString( result );
+		assertThat( uuid.length() ).isEqualTo( 35 );
+		String[] splitUUID = uuid.split( "-" );
+		assertThat( splitUUID.length ).isEqualTo( 4 );
+		assertThat( splitUUID[ 0 ].length() ).isEqualTo( 8 );
+		assertThat( splitUUID[ 1 ].length() ).isEqualTo( 4 );
+		assertThat( splitUUID[ 2 ].length() ).isEqualTo( 4 );
+		assertThat( splitUUID[ 3 ].length() ).isEqualTo( 16 );
+	}
+
+	@DisplayName( "It uses the hexadecimal character set" )
+	@Test
+	public void testIsHexadecimal() {
+		instance.executeSource(
+		    """
+		    result = createUUID();
+		    """,
+		    context );
+
+		String uuid = variables.getAsString( result );
+		assertThat( uuid.matches( "[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{16}" ) ).isEqualTo( true );
+	}
+
+	@DisplayName( "It forces uppercase characters" )
+	@Test
+	public void testIsForcedUppercase() {
+		instance.executeSource(
+		    """
+		    result = createUUID();
+		    """,
+		    context );
+
+		String uuid = variables.getAsString( result );
+		assertThat( uuid.matches( ".*[a-f].*" ) ).isEqualTo( false );
+	}
+
+}
