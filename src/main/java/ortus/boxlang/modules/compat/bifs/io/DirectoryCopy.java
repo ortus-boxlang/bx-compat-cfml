0186fd1976f7fd8011a4d08d2296cf281c3eed30
Brad Wood <brad@bradwood.com>
2026-01-23 14:16:37 -0600

    BL-2124

---
+++ src/main/java/ortus/boxlang/modules/compat/bifs/io/DirectoryCopy.java
@@ -0,0 +1,42 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package ortus.boxlang.modules.compat.bifs.io;
+
+import java.security.Key;
+
+import ortus.boxlang.runtime.bifs.BoxBIF;
+import ortus.boxlang.runtime.types.Argument;
+
+@BoxBIF
+
+public class DirectoryCopy extends ortus.boxlang.runtime.bifs.global.io.DirectoryCopy {
+
+	/**
+	 * Constructor
+	 */
+	public DirectoryCopy() {
+		super();
+		declaredArguments = new Argument[] {
+		    new Argument( true, "string", Key.source ),
+		    new Argument( true, "string", Key.destination ),
+		    new Argument( false, "boolean", Key.recurse, false ),
+		    new Argument( false, "any", Key.filter, "*" ),
+		    new Argument( false, "boolean", Key.createPath, true ),
+		    new Argument( false, "boolean", Key.overwrite, true )
+		};
+	}
+
+}
